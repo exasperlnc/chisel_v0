6bab1e7982fb6000055fd96ef45853d68f79eb77
Logan Cole <emtlogancole@gmail.com>
2024-04-01 13:36:25 -0500

    Conditionally run program

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -10,14 +10,17 @@ end
 markdown_file = ARGV[0]
 html_file = ARGV[1]
 
-# read the input markdown
-markdown = File.read(markdown_file)
-# convert it to html
-html = Chisel.new(markdown).to_html
-# write the html to the output file
-File.write(html_file, html)
-# print this summary
-num_lines_markdown = markdown.lines.count
-num_lines_html = html.lines.count
+program_running = ($PROGRAM_NAME == __FILE__)
+if program_running
+  # read the input markdown
+  markdown = File.read(markdown_file)
+  # convert it to html
+  html = Chisel.new(markdown).to_html
+  # write the html to the output file
+  File.write(html_file, html)
+  # print this summary
+  num_lines_markdown = markdown.lines.count
+  num_lines_html = html.lines.count
 
-puts "Converted #{markdown_file} (#{num_lines_markdown} lines) to #{html_file} (#{num_lines_html} lines)"
+  puts "Converted #{markdown_file} (#{num_lines_markdown} lines) to #{html_file} (#{num_lines_html} lines)"
+end
