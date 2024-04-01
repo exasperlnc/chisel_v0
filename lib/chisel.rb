f4a242bd1678ef7556af2a3afaf959f22059f5f3
Logan Cole <emtlogancole@gmail.com>
2024-04-01 13:00:11 -0500

    pseudocode

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,6 +1,8 @@
 # read the input markdown
 markdown = File.read(markdown_file)
 # convert it to html
+html = Chisel.new(markdown).to_html
 # write the html to the output file
+File.write(html_file, html)
 # print this summary
-
+puts "Converted #{markdown_file} (#{num_lines_markdown} lines) to #{html file} (#{num_lines_html} lines)"
