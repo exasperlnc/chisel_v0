486d8a21c10341eea72320f4be20f789e423c748
Logan Cole <emtlogancole@gmail.com>
2024-04-01 13:31:25 -0500

    Implement toplevel Chisel

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,3 +1,15 @@
+class Chisel
+  def initialize(markdown)
+    @markdown = markdown
+  end
+
+  def to_html
+    @markdown.reverse
+  end
+end
+markdown_file = ARGV[0]
+html_file = ARGV[1]
+
 # read the input markdown
 markdown = File.read(markdown_file)
 # convert it to html
@@ -5,4 +17,7 @@ html = Chisel.new(markdown).to_html
 # write the html to the output file
 File.write(html_file, html)
 # print this summary
-puts "Converted #{markdown_file} (#{num_lines_markdown} lines) to #{html file} (#{num_lines_html} lines)"
+num_lines_markdown = markdown.lines.count
+num_lines_html = html.lines.count
+
+puts "Converted #{markdown_file} (#{num_lines_markdown} lines) to #{html_file} (#{num_lines_html} lines)"
